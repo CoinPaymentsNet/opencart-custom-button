ea0723a1ea97b6988a596f3f5734ecd298610670
NastyaGos <72504315+NastyaGos@users.noreply.github.com>
2021-03-23 06:20:26 -0700

    api v2 test release

---
+++ upload/admin/language/en-gb/extension/module/coinpayments_button.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Coinpayments Checkout Button';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified Coinpayments Checkout Button module!';
+$_['text_edit']        = 'Edit Coinpayments Checkout Button Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Coinpayments Checkout Button module!';
---
+++ upload/catalog/language/en-gb/extension/module/coinpayments_button.php
@@ -0,0 +1,7 @@
+<?php
+//Heading
+$_['heading_title']    = 'Pay via Coinpayments';
+
+//Text
+$_['text_description'] = 'Customers please click Coinpayments button to complete checkout';
+?>

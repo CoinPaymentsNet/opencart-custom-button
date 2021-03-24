a6dd8fca0082f91edb7ec5c5d776b33cc22986b2
NastyaGos <72504315+NastyaGos@users.noreply.github.com>
2021-03-24 06:21:54 -0700

    api v2 test release

--- upload/catalog/controller/extension/module/coinpayments_button.php
+++ upload/catalog/controller/extension/module/coinpayments_button.php
@@ -9,11 +9,15 @@ class ControllerExtensionModuleCoinpaymentsButton extends Controller {
         }
         $data['total_price'] = number_format($this->cart->getTotal(), 2, '', '');
 
+        $this->coinpayments = new Coinpayments($this->registry);
+        $coin_currency = $this->coinpayments->getCoinCurrency($this->config->get('config_currency'));
+        $data['currency_id'] = $coin_currency['id'];
+
         $data['items'] = array();
         foreach ($this->cart->getProducts() as $product) {
-            array_push($data['items'], array("name"=>$product['name'], "quantity"=>array("value"=>$product['quantity'], "type"=>"1"), "amount"=>strval(number_format($product['total'], 2, '', ''))));
+            array_push($data['items'], array("name"=>$product['name'], "quantity"=>array("value"=>$product['quantity'], "type"=>"1"), "amount"=>strval(number_format($product['total'], $coin_currency['decimalPlaces'], '', ''))));
         }
-        $data['taxes'] = json_encode(array("subtotal"=>strval( number_format($this->cart->getSubTotal(), 2, '', '')),"taxTotal"=>strval(number_format(array_sum($this->cart->getTaxes()), 2, '', ''))));
+        $data['taxes'] = json_encode(array("subtotal"=>strval( number_format($this->cart->getSubTotal(), $coin_currency['decimalPlaces'], '', '')),"taxTotal"=>strval(number_format(array_sum($this->cart->getTaxes()), $coin_currency['decimalPlaces'], '', ''))));
         $data['items'] = json_encode($data['items']);
         $data['client_id'] = json_encode($this->config->get('payment_coinpayments_client_id'));
         if ($status) {
